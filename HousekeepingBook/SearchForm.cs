76e4786de9ea0294154582f84e55e075b48be89a
sone <sone@inet88.co.jp>
2017-11-21 15:24:14 +0900

    年月日を選択をコメント化

--- HousekeepingBook/SearchForm.cs
+++ HousekeepingBook/SearchForm.cs
@@ -29,35 +29,35 @@ namespace HousekeepingBook
         {
             Console.WriteLine(dateTimePicker1.Value.ToShortDateString());
 
-            // 入力ファイル名
-            string path = "MoneyData.csv";
-            // 区切り文字
-            string delimStr = ",";
-            // 区切り文字をまとめる
-            char[] delimiter = delimStr.ToCharArray();
-            // 分解後の入れ物
-            string[] strData;
-            // 1行分のデータ
-            string strLine;
-            bool fileExists = System.IO.File.Exists(path);
-            if (fileExists)
-            {
-                System.IO.StreamReader sr = new System.IO.StreamReader(
-                    path,
-                    System.Text.Encoding.Default);
-                while (sr.Peek() >= 0)
-                {
-                    strLine = sr.ReadLine();
-                    strData = strLine.Split(delimiter);
-                    moneyDataSet.moneyDataTable.AddmoneyDataTableRow(
-                        DateTime.Parse(strData[0]),
-                        strData[1],
-                        strData[2],
-                        int.Parse(strData[3]),
-                        strData[4]);
-                }
-                sr.Close();
-            }
+            //// 入力ファイル名
+            //string path = "MoneyData.csv";
+            //// 区切り文字
+            //string delimStr = ",";
+            //// 区切り文字をまとめる
+            //char[] delimiter = delimStr.ToCharArray();
+            //// 分解後の入れ物
+            //string[] strData;
+            //// 1行分のデータ
+            //string strLine;
+            //bool fileExists = System.IO.File.Exists(path);
+            //if (fileExists)
+            //{
+            //    System.IO.StreamReader sr = new System.IO.StreamReader(
+            //        path,
+            //        System.Text.Encoding.Default);
+            //    while (sr.Peek() >= 0)
+            //    {
+            //        strLine = sr.ReadLine();
+            //        strData = strLine.Split(delimiter);
+            //        moneyDataSet.moneyDataTable.AddmoneyDataTableRow(
+            //            DateTime.Parse(strData[0]),
+            //            strData[1],
+            //            strData[2],
+            //            int.Parse(strData[3]),
+            //            strData[4]);
+            //    }
+            //    sr.Close();
+            //}
 
         }
     }
